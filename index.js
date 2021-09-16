db63a6be951fe47aca438aea3d1aba2a3457bc5d
jkworks <info@jilliankayworks.com>
2021-09-15 19:49:48 -0600

    added switch case

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const chooseSection = ()=> {
         message: 'Choose from the following options:',
         choices: [
             "view all departments",
-            "view all departments",,
+            "view all roles",
             "view all employees",
             "add a department",
             "add a role",
@@ -26,19 +26,36 @@ const chooseSection = ()=> {
             case "view all departments":
               //open view all dept
               //result of expression matches value1
-            //   [break;]
-            case "view all departments":
+            break;
+            case "view all roles":
               //open all roles
               //result of expression matches value2
-            //   [break;]
+              break;
             case "add a role":
               //open all employee
             //   //result of expression matches valueN
-            //   [break;]
+            break;
+            case "view all employees":
             // [default:
             //   //Statements executed when none of
             //   //the values match the value of the expression
-            //   [break;]]
+            //   [break;]
+            case "add a department":
+                   //open all employee
+            //   //result of expression matches valueN
+            break;
+            case "add a role":
+                   //open all employee
+            //   //result of expression matches valueN
+            break;
+            case "add an employee":
+                   //open all employee
+            //   //result of expression matches valueN
+            break;
+            case "add an employee role":
+                   //open all employee
+            //   //result of expression matches valueN
+            break;
           }
       }
     );
@@ -80,3 +97,6 @@ const addRole = ()=> {
         //add to SQL Database
       });
 }
+
+
+chooseSection();
